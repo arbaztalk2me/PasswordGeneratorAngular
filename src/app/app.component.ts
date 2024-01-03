6d68516e0f53cfbfd1eed7b9973cc70841217f4a
arbaztalk2me <geekyarbaz@gmail.com>
2024-01-03 23:46:33 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'randompass';
+  length=0;
+  password:string=""
+  letters:boolean=false;
+  digit:boolean=false;
+  symbol:boolean=false;
+
+  handleChangeText(event:any){
+    let getnum=parseInt(event.target.value);
+    if(!isNaN(getnum)){
+      this.length=getnum
+    }else{
+      this.length=0
+    }
+   
+    
+  }
+
+  handleLetter(){
+    this.letters=!(this.letters)
+  }
+
+  handleDigit(){
+    this.digit=!(this.digit)
+  }
+  handleSymbol(){
+    this.symbol=!(this.symbol)
+  }
+
+  handleSubmit(){
+    this.password=""
+    let genpass="";
+    let alpha="abcdefghijklmnopqrstuvwxyz";
+    let dig="1234567890";
+    let sym="!@#$%^&*()";
+    if(this.letters){
+      genpass+=alpha
+    }
+    if(this.digit){
+      genpass+=dig;
+    }
+    if(this.symbol){
+      genpass+=sym
+    }
+console.log(genpass)
+    for(let i=0;i<this.length;i++){
+      console.log(Math.floor(Math.random()*genpass.length))
+      this.password+=genpass[Math.floor(Math.random()*genpass.length)]
+    }
+
+
+  }
+
+
 }
